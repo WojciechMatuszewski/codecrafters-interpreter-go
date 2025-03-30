0af59eaacece039fb57d65b320517dbcabaf4847
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2025-03-30 10:58:20 +0200

    update

--- app/main.go
+++ app/main.go
@@ -41,33 +41,6 @@ func main() {
 
 type TokenType string
 
-func (t TokenType) Format() string {
-	switch t {
-	case LEFT_PAREN:
-		{
-			return fmt.Sprintf("LEFT_PAREN %v null\n", t)
-		}
-	case RIGHT_PAREN:
-		{
-			return fmt.Sprintf("RIGHT_PAREN %v null\n", t)
-		}
-	case LEFT_BRACE:
-		{
-			return fmt.Sprintf("LEFT_BRACE %v null\n", t)
-		}
-	case RIGHT_BRACE:
-		{
-			return fmt.Sprintf("RIGHT_BRACE %v null\n", t)
-		}
-	case EOF:
-		{
-			return "EOF  null\n"
-		}
-	}
-
-	return ""
-}
-
 const (
 	// Single-character tokens
 	LEFT_PAREN  TokenType = "("
@@ -127,11 +100,13 @@ func NewLox() *Lox {
 func (l *Lox) Run(r io.Reader, w io.Writer) error {
 	reader := bufio.NewReader(r)
 
-	tokens := []TokenType{}
+	output := ""
+
 	for {
 		b, err := reader.ReadByte()
 		if err != nil {
 			if errors.Is(err, io.EOF) {
+				output += "EOF  null\n"
 				break
 			}
 
@@ -142,29 +117,51 @@ func (l *Lox) Run(r io.Reader, w io.Writer) error {
 		switch token {
 		case string(LEFT_BRACE):
 			{
-				tokens = append(tokens, LEFT_BRACE)
+				output += fmt.Sprintf("LEFT_BRACE %v null\n", LEFT_BRACE)
 			}
 		case string(RIGHT_BRACE):
 			{
-				tokens = append(tokens, RIGHT_BRACE)
+				output += fmt.Sprintf("RIGHT_BRACE %v null\n", RIGHT_BRACE)
 			}
 		case string(LEFT_PAREN):
 			{
-				tokens = append(tokens, LEFT_PAREN)
+				output += fmt.Sprintf("LEFT_PAREN %v null\n", LEFT_PAREN)
 			}
 		case string(RIGHT_PAREN):
 			{
-				tokens = append(tokens, RIGHT_PAREN)
+				output += fmt.Sprintf("RIGHT_PAREN %v null\n", RIGHT_PAREN)
+			}
+		case string(COMMA):
+			{
+				output += fmt.Sprintf("COMMA %v null\n", COMMA)
+			}
+		case string(DOT):
+			{
+				output += fmt.Sprintf("DOT %v null\n", DOT)
+			}
+		case string(MINUS):
+			{
+				output += fmt.Sprintf("MINUS %v null\n", MINUS)
+			}
+		case string(PLUS):
+			{
+				output += fmt.Sprintf("PLUS %v null\n", PLUS)
+			}
+		case string(SEMICOLON):
+			{
+				output += fmt.Sprintf("SEMICOLON %v null\n", SEMICOLON)
+			}
+		case string(SLASH):
+			{
+				output += fmt.Sprintf("SLASH %v null\n", SLASH)
+			}
+		case string(STAR):
+			{
+				output += fmt.Sprintf("STAR %v null\n", STAR)
 			}
 		default:
 			return fmt.Errorf("token %v not implemented", token)
 		}
-	}
-
-	tokens = append(tokens, EOF)
-	output := ""
-	for _, token := range tokens {
-		output += token.Format()
 	}
 
 	_, err := w.Write([]byte(output))
